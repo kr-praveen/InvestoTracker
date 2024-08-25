9b1393a33e3e0f993acf44ba80b40736e66f4915
Praveen Kumar Verma <kr.praveen.infinity@gmail.com>
2024-08-25 07:43:09 +0530

    routing done + basic version created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './core/landing-page/landing-page.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { LoginErrorComponent } from './error/login-error/login-error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'error/login-error',
+        component: LoginErrorComponent
+    }
+];
---
+++ src/app/error/login-error/login-error.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-error',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-error.component.html',
+  styleUrl: './login-error.component.css'
+})
+export class LoginErrorComponent {
+
+}
